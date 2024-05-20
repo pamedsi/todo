9819b83bfe282df1f1b7dc7f850325f63b0c60a9
Patrick Melo <patrickabimael@yahoo.com.br>
2024-05-20 17:39:36 -0300

    termina testes de controller

--- src/test/java/com/clarotodo/controller/TarefaControllerTest.java
+++ src/test/java/com/clarotodo/controller/TarefaControllerTest.java
@@ -2,6 +2,7 @@ package com.clarotodo.controller;
 
 import com.clarotodo.dto.*;
 import com.clarotodo.entity.*;
+import com.clarotodo.service.*;
 import com.fasterxml.jackson.databind.*;
 
 import org.junit.jupiter.api.*;
@@ -9,18 +10,23 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.autoconfigure.web.servlet.*;
 import org.springframework.boot.test.context.*;
+import org.springframework.boot.test.mock.mockito.*;
 import org.springframework.http.*;
 import org.springframework.test.web.servlet.*;
 import org.springframework.test.web.servlet.request.*;
 import org.springframework.test.web.servlet.result.*;
 
 import static com.clarotodo.DataHelper.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class TarefaControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private TarefaService tarefaService;
     private final TarefaRequest tarefaDTO = criarTarefaDTO();
     private final Tarefa tarefa = criarTarefa();
 
@@ -36,6 +42,7 @@ class TarefaControllerTest {
 
     @Test
     void deveDetalharTarefa() throws Exception {
+        when(tarefaService.buscarTarefa(any())).thenReturn(criarDetalhesDaTarefa());
         mockMvc.perform(MockMvcRequestBuilders.get("/tarefa/" + tarefa.getIdentificador().toString())
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
