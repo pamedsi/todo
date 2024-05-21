1ceafcad3a406b06399067b813b5096160536610
Patrick Melo <patrickabimael@yahoo.com.br>
2024-05-21 18:08:51 -0300

    define constraint de tamanho do título

--- src/main/java/com/clarotodo/dto/TarefaRequest.java
+++ src/main/java/com/clarotodo/dto/TarefaRequest.java
@@ -1,9 +1,11 @@
 package com.clarotodo.dto;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.*;
 
 public record TarefaRequest(
         @NotBlank (message = "Campo 'titulo' não pode estar vazio.")
+        @Length(max = 255, message = "Titulo não pode ser maior que 255 caracteres.")
         String titulo,
         @NotBlank (message = "Campo 'descricao' não pode estar vazio")
         String descricao
