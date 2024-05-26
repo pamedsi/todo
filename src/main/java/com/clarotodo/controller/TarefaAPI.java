8df01011d44c4de8a9c3594e4ce2dbfc9ac84df7
Patrick Melo <patrickabimael@yahoo.com.br>
2024-05-26 12:44:00 -0300

    acrescenta /api no endpoint antes de /tarefa

--- src/main/java/com/clarotodo/controller/TarefaAPI.java
+++ src/main/java/com/clarotodo/controller/TarefaAPI.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/tarefa")
+@RequestMapping("/api/tarefa")
 public interface TarefaAPI {
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
